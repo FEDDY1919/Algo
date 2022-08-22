a92e7335d4a9acda107c5ab09a0dc3038bee3ed1
FEDDY1919 <72752593+FEDDY1919@users.noreply.github.com>
2022-08-23 00:28:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -91,11 +91,11 @@ if __name__ == "__main__":
 
 '''
 Things to Consider:
-1. Size of the robot, the path created would only work if the robot is 10cm x 10cm
+1. Size of the robot, the path created would only work if the robot is 10cm x 10cm and can travel omni-directionally
 2. Points at which the robot should stop, it should not be directly on the obstacle, but near it (done)
 3. Include in the heuristic turn penalties and such, making a left or right turn would cause more movement than travelling a straight line
-4. Include current orientation within the current_node to help in point 3
-5. ....
+4. Include current orientation within the current_node to help in point 3(somewhat done)
+5. Possible include different waypoints if image recognition is good
 
 [r][c] row of the grid, column of the grid
 
@@ -108,7 +108,43 @@ Things to Consider:
 [r-1][c-1] a NORTHWEST movement
 [r+1][c-1] a SOUTHWEST movement
 [r-1][c+1] a NORTHEAST movement
+===================================================================================
+Point 3.The robots orientation matters in the efficiency of pathfinding
+Consider the following situation:
+      T1(N)
+     
+O            T2(E)
 
+O represents the robot, T(n) represents the the point of interest for the robot and the direction the robot needs to face
+If the robot is currently facing East, it should aim for T2 instead of T1 first.
+Not necessarily is the closest waypoint the most convenient for the current robot.
+
+The way the current Pathfinding algorithm we have works:
+1.Iterate through the position of all obstacles.
+2.Create the target positions for the obstacles.
+3.Out of all of the target positions, travel to the cloest one.
+4.Sequentially travel to the remaining positions in a clockwise manner.
+
+It could be possible to add into the simple hamiltonian path to include orientation into the distance calculation.
+We would generate all possible waypoints which the obstacle possesses and the direction the car needs to face when on the waypoint.
+For North, it would be better if the obstacle possesses a lower [r] value with a N waypoint
+For East, obstacle higher [c] value with E waypoint
+For West, obstacle lower [c] with W waypoint
+For South, obstacle higher [r] value with S waypoint
+
+Taking this into consideration, the robot would be less likely to perform a U turn maneuver, we have to find out the distance the robot travels
+when it does a U turn, left turn and right turn and include it into the calculation if possible.
+===================================================================================
+Point 5. The camera can recognise 2 images simultaneously
+Currently, the idea would be for the robot to travel N,S,E,W of the obstacle to identify one image at a time.
+That would be 4 positions, however we can cut this down to 3.
+Currently, we use [0,3],[0,-3],[3,0],[-3,0]
+
+Instead, we can use[-3,-3],[-3,3],[3,-3],[3,3]
+We will find the closest position and travel to it, after which we travel to the other 2 positions.
+The sequence in which it would do that would be refined from Point 3.
+
+===================================================================================
 PseudoCode for if image is found:
     while scanning:
         if img_id == bullseye_id:
@@ -117,6 +153,8 @@ PseudoCode for if image is found:
         else:
             save id
             break and continue to next obstacle
+
+
 
 '''
 
